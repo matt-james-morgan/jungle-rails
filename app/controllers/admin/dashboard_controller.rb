2d2ca9e1407338f8ac3ea3f68d6b6ab34d87c076
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-04-14 16:08:22 -0400

    added counts on dashbaord

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate
 
   def show
+    @total_products = Product.sum(:quantity)
+    @total_categories = Category.count
   end
 
  
