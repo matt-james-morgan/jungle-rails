5909b3b5849a720ff2b748fa556d5776607e605a
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-04-14 15:34:28 -0400

    made descriptive checkout page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @line_items = @order.line_items.includes(:product)
+
+   
   end
 
   def create
