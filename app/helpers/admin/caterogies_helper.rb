e7e2161f69a47e53f49e7d08cb21cba31b69afa7
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-04-15 11:25:28 -0400

    added crud functions to category dashboard

---
+++ app/helpers/admin/caterogies_helper.rb
@@ -0,0 +1,2 @@
+module Admin::CaterogiesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
+    # get 'categories', to: 'categories#index'
   end
 
   get 'about', to: 'about#index'
